35ad42b72c7b060e77f9592a6a07506a95e619ce
evergreenies <evergreenlief@gmail.com>
2022-04-19 06:50:55 +0530

    Updated `balanced_parenthesis.py`

--- recursion/balanced_parenthesis.py
+++ recursion/balanced_parenthesis.py
@@ -36,7 +36,38 @@ def balanced_parenthesis(opn: int, close: int, output: str, arr: List) -> None: 
         balanced_parenthesis(opn, close - 1, output2, arr)
 
 
+def balanced_parenthesis1(n: int) -> List:
+    """
+    Balanced parenthesis
+
+    :param n:
+    :type n:
+    :return:
+    :rtype:
+    """
+
+    def rec(index: int, diff: int, comb: List, combs: List) -> None:
+        """Recursive Function"""
+        if (diff < 0) or (diff > index):
+            return
+        elif index == 0:
+            if diff == 0:
+                combs.append(''.join(comb))
+        else:
+            comb.append('(')
+            rec(index - 1, diff + 1, comb, combs)
+            comb.pop()
+            comb.append(')')
+            rec(index - 1, diff - 1, comb, combs)
+            comb.pop()
+
+    combinations = []
+    rec(2 * n, 0, [], combinations)
+    return combinations
+
+
 if __name__ == '__main__':
     arr = []
     balanced_parenthesis(3, 3, '', arr)
     print(arr)
+    print(balanced_parenthesis1(3))
